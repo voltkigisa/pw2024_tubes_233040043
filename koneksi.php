73441566e0ecf3a80d451a595c97eb6183d17841
voltkigisa <raden.233040043@mail.mail.ac.id>
2024-05-11 16:03:31 +0700

    ganti nama data base

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = new mysqli("localhost", "root", "", "yasumiport");
+$koneksi = new mysqli("localhost", "root", "", "pw2024_tubes_233040043");
 if ($koneksi->connect_error) {
     die("Koneksi ke database gagal: " . $koneksi->connect_error);
 }
