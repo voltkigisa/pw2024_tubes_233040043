5672cb86bdf09bd57840bf750f78a1e3bd38e40c
voltkigisa <raden.233040043@mail.mail.ac.id>
2024-05-15 10:07:57 +0700

    profile

--- views/profile/profile.php
+++ views/profile/profile.php
@@ -1,1 +1,19 @@
-<?php include '../../admin/layout/navbar.php' ?>
+<?php include '../../admin/layout/navbar.php';
+$userProfile = [
+    'username' => $_SESSION['username'],
+    'email' => 'user@example.com', // Sesuaikan dengan data dari database
+    'role' => $_SESSION['role'],
+    'profile_image' => 'path/to/profile_image.jpg' // Path ke gambar profil
+];
+
+// Jika data pengguna tidak ditemukan, arahkan ke halaman login
+if (!isset($_SESSION['status']) || $_SESSION['status'] !== 'login') {
+    header('Location: /pw2024_tubes_233040043/views/login/login.php');
+    exit();
+}
+?>
+
+
+
+</body>
+</html>
